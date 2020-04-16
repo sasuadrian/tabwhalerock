e87d6f18e9e318d9ecd39783df147c38abb17b72
adriansasu <sasuadrian12@gmail.com>
2020-04-17 02:20:45 +0300

    updating the the shop page, collection preview component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shoppage/shop.component';
+
+import './App.css';
+  
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
