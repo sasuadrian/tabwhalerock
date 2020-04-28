6159af6cb3819b8d55d7424cf022171058b12962
adriansasu <dida.dyda18@gmail.com>
2020-04-28 10:37:16 +0300

    Updates

---
+++ precache-manifest.8d876398817c85fcdcd663a8d078a9dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e6edcb2b59ce3a79646",
+    "url": "/tabwhalerock/static/css/main.5b66ee86.chunk.css"
+  },
+  {
+    "revision": "5e6edcb2b59ce3a79646",
+    "url": "/tabwhalerock/static/js/main.6cffdf87.chunk.js"
+  },
+  {
+    "revision": "859273d474adce417afb",
+    "url": "/tabwhalerock/static/js/runtime~main.94138f8e.js"
+  },
+  {
+    "revision": "e0a194f1925ea8c3a1fb",
+    "url": "/tabwhalerock/static/js/2.6bc24cce.chunk.js"
+  },
+  {
+    "revision": "96a740b7dc219a4ad0c2074392315ab4",
+    "url": "/tabwhalerock/static/media/logo.96a740b7.png"
+  },
+  {
+    "revision": "6ed52683fcf4e2c440e1dd68b881d17a",
+    "url": "/tabwhalerock/index.html"
+  }
+];
